2b523d09edcba118ca9b44cfb19a9ded2451b29d
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-08-02 04:24:05 +0000

    コメント編集可能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  
+  # GET /comments/1/edit
+  def edit
+    @edit = true
+  end
+  
+   # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to topic_path(@comment.topic_id), notice: '更新いたしました。' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
   def create
     # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
@@ -19,7 +39,7 @@ class CommentsController < ApplicationController
   end
   
   def destroy
-    @comment = Comment.find(params[:id])
+    #@comment = Comment.find(params[:id])
     @comment.destroy
     respond_to do |format|
       format.html { redirect_to topic_path(@comment.topic), notice: 'Comment was successfully destroyed.' }
@@ -35,7 +55,9 @@ class CommentsController < ApplicationController
       params.require(:comment).permit(:topic_id, :content)
     end
     
-
-    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
     
 end
