d3ce4dfb29744e07352601a83835c721cc69dd4e
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-08-04 04:31:54 +0000

    heroku up

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'omniauth-facebook'
 
 gem 'carrierwave'
 gem 'rmagick'
+
+gem 'pusher'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,31 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    
+    #自分がフォローしている人を取得
+    @my_followed_users =current_user.followed_users
+    #binding.pry
+    
+    #自分をフォローしている人を取得
+    @my_followers=current_user.followers
   end
   
-  def show
-    @user = User.find(params[:id])
-    @followed_users= current_user.followed_users
-    @followers=current_user.followers
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  #   @followed_users= current_user.followed_users
+  #   @followers=current_user.followers
+  # end
 
   def show_user
     @user = User.find(params[:id])
+    
+    #フォローしている人を取得
     @followed_users= @user.followed_users  
 
+    #フォロワーを取得
     @followers=@user.followers
     # binding.pry
+    
   end
   
 end
