8ee810270ea274a40ac84d57b6d4c8993547adc1
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-08-16 06:59:43 +0000

    本番mail設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   # authentication: :plain,
   # enable_starttls_auto: true
   # }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
