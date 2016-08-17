c29eccf669eeb58cd2ceb1f04b08d0015810ce63
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-08-17 04:52:54 +0000

    ゴミ削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,42 @@
-10.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
+# 10.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(
+#               name: "Test Diver#{n}",
+#               email: email,
+#               password: password,
+#               password_confirmation: password,
+#               uid: "#{n}"
+#               )
+# end
+
+
+# 3.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   name = Faker::Name.name
+#   uid = Faker::Code.ean
+#   provider = Faker::Company.name
+#   #image_url = File.open(Dir.glob(File.join(Rails.root, 'sampleusers', '*')).sample)
+#   User.create!(email: email,
+#               name: name,
+#               password: password,
+#               password_confirmation: password,
+#               uid: uid,
+#               provider: provider,
+#               #image_url: image_url,
+#               #avatar: image_url,
+#               )
+
+# user = User.all.order(id: :desc).first
+# 3.times do |n|
+#     user_id = user.id
+#     photo = File.open(Dir.glob(File.join(Rails.root, 'sampleimages', '*')).sample)
+#     content = Faker::Lorem.sentence
+#   Topic.create!(user_id: user_id,
+#               photo: photo,
+#               content: content,
+#               )
+# end
+
+# end
