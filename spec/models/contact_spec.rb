9315c2a49b0ea8c2a0a30235e200645ab93d90bc
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-08-21 14:42:00 +0000

    rspec設定

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Contactモデルバリデーションテスト' do
+
+   it "nameの空白は無効" do
+     contact = Contact.new(name: nil, email: 'hogehoge@hoge.com',message: 'hogehoge')
+     #expect(contact).not_to be_valid
+     contact.valid?
+     expect(contact.errors[:name]).to include("名前を入力してください")
+   end
+   
+   it "emailの空白は無効" do
+     contact = Contact.new(name: 'hoge', email: nil ,message: 'hogehoge')
+     contact.valid?
+     expect(contact.errors[:email]).to include("メールアドレスを入力してください")
+   end
+   
+   it "messageの空白は無効" do
+     contact = Contact.new(name: 'hoge', email: 'hogehoge@hoge.com' ,message: nil)
+     contact.valid?
+     expect(contact.errors[:message]).to include("お問い合わせ内容を入力してください")
+   end
+
 end
